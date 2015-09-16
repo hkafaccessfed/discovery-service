55d2be642f1eaaa9b03d8de3fc96ecc23c5616b6
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-09-16 11:02:41 +1000

    Added logging to Sinatra app (stdout + file)
    Taken from http://recipes.sinatrarb.com/p/middleware/rack_commonlogger

--- app/discovery_service/application.rb
+++ app/discovery_service/application.rb
@@ -3,6 +3,13 @@ require 'sinatra/base'
 module DiscoveryService
   # Web application to allow users to select their IdP
   class Application < Sinatra::Base
+    configure do
+      enable :logging
+      file = File.new("log/#{settings.environment}.log", 'a')
+      file.sync = true
+      use Rack::CommonLogger, file
+    end
+
     get '/' do
       slim :index
     end
