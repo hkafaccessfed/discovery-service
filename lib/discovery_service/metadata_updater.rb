9e0ca069b8ae4d1b37032e57f103c846ba9073b8
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-09-21 11:19:57 +1000

    Added integration test for MetadataUpdater
    Designed to simulate running the ruby script (`ruby
    bin/update_metadata.rb`).

--- lib/discovery_service/metadata_updater.rb
+++ lib/discovery_service/metadata_updater.rb
@@ -16,8 +16,7 @@ module DiscoveryService
     def update
       config = YAML.load_file('config/discovery_service.yml')
       entity_data = retrieve_entity_data(config[:saml_service][:uri])
-      filtered_entities = filter(entity_data[:entities], config[:collections])
-      puts "\nNOW CACHE THIS: #{JSON.pretty_generate(filtered_entities)}"
+      filter(entity_data[:entities], config[:collections])
     end
   end
 end
