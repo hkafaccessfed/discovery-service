816f087f5d2f478c809ddb0397efd2dd42a45eb9
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-10-02 11:07:24 +1000

    Setting I18n local as random local errors appearing again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,9 @@ require 'fakeredis/rspec'
 require 'discovery_service/application'
 Capybara.app = DiscoveryService::Application
 
+I18n.config.enforce_available_locales = true
+I18n.config.default_locale = :en
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
