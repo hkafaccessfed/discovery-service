e157e4a50d6b8bb452991f9bbccbcdf8bca56734
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-10-20 12:15:14 +1000

    Added tests for EntityCache class
    A new class emerged from the last refactor — now filling in the missing
    tests.

--- spec/support/build_entity_data.rb
+++ spec/support/build_entity_data.rb
@@ -1,16 +1,17 @@
 require 'active_support/core_ext/hash'
 
 RSpec.shared_context 'build_entity_data' do
-  def build_entity_data(tags)
+  def build_entity_data(tags = nil)
     {
       entity_id: Faker::Internet.url,
       discovery_response: Faker::Internet.url,
       name: Faker::Company.name,
-      tags: tags
+      tags: tags.nil? ? [Faker::Lorem.word, Faker::Lorem.word] : tags
     }
   end
 
   def to_hash(entities)
-    Hash[entities.map { |e| [e[:entity_id], e.except(:entity_id)] }]
+    hash = Hash[entities.map { |e| [e[:entity_id], e.except(:entity_id)] }]
+    hash.each { |_k, v| v.symbolize_keys! }
   end
 end
