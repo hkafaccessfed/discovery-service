a4fa2afa0d9e47c4d3326c052acd550acf12f512
Russell Ianniello <russell.ianniello@aaf.edu.au>
2015-11-18 11:22:23 +1000

    CSS (row) pointer cursor disabled for HTML version

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -9,19 +9,27 @@ $(document).ready(function () {
     $('#search_clear_button').on('click', function () {
       clearSearch();
     });
+
     $('#organisations_near_me_button').on('click', function () {
       getLocation();
     });
   }
 
   function hideNonJSElements() {
+    // Hide buttons alongside each IdP (for HTML only)
     $('.select_organisation_button').hide();
   }
 
   function showJSEnabledElements() {
+    // Search only works when JS enabled, so show it
     $('#search_options').show();
+
+    // Display the main "Select" button below the list
     $('#select_organisation_button').css("display", "inline-block");
     $('#select_organisation_button').text('Select');
+
+    // We only want the pointer icon to appear on IdP rows when JS enabled
+    $('.datatable tbody tr').css('cursor', 'pointer');
 
     $('#sp_header').text('');             // TODO Set Initiating SP
     $('#sp_header_logo').attr("src", ''); // TODO Set Initiating SP
