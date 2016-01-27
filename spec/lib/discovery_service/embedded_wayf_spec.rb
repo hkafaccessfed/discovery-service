21516a5746632e3b9ca4af1638d2e4a03c47a8dd
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-01-27 10:47:06 +1000

    Ensure only IdPs are listed in embedded WAYF

--- spec/lib/discovery_service/embedded_wayf_spec.rb
+++ spec/lib/discovery_service/embedded_wayf_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe DiscoveryService::EmbeddedWAYF do
 
   describe '#embedded_wayf_javascript' do
     let(:output) { subject.embedded_wayf_javascript }
+    let(:rendered_json) { /var idp_entities = (\[.*?\]);/m.match(output)[1] }
+    let(:rendered_entities) { JSON.parse(rendered_json, symbolize_names: true) }
 
     let(:expected_entities) do
       [
@@ -32,13 +34,18 @@ RSpec.describe DiscoveryService::EmbeddedWAYF do
       ]
     end
 
+    let(:tagged_entities) do
+      expected_entities.map { |e| e.merge(tags: %w(idp)) }
+    end
+
     let(:entities_as_hash) do
-      expected_entities.reduce({}) do |a, e|
+      tagged_entities.reduce({}) do |a, e|
         attrs = {
           names: [
             { value: 'wrong', lang: 'fr' },
             { value: e[:name], lang: 'en' }
-          ]
+          ],
+          tags: e[:tags]
         }
         a.merge(e[:entity_id] => attrs)
       end
@@ -54,27 +61,46 @@ RSpec.describe DiscoveryService::EmbeddedWAYF do
     end
 
     it 'includes the entities' do
-      json = /var idp_entities = (\[.*?\]);/m.match(output)[1]
-      expect(JSON.parse(json, symbolize_names: true))
-        .to eq(expected_entities)
+      expect(rendered_entities).to eq(expected_entities)
     end
 
     context 'with a missing name' do
       let(:entities_as_hash) do
-        expected_entities.reduce({}) do |a, e|
+        tagged_entities.reduce({}) do |a, e|
           attrs = {
             names: [
               { value: 'wrong', lang: 'fr' }
-            ]
+            ],
+            tags: e[:tags]
           }
           a.merge(e[:entity_id] => attrs)
         end
       end
 
       it 'uses the entity id as the name' do
-        json = /var idp_entities = (\[.*?\]);/m.match(output)[1]
-        expect(JSON.parse(json, symbolize_names: true))
+        expect(rendered_entities)
           .to eq(expected_entities.map { |e| e.merge(name: e[:entity_id]) })
+      end
+    end
+
+    context 'with an SP' do
+      let(:extra_entities) do
+        [
+          {
+            entity_id: 'http://sp.example.edu/shibboleth',
+            name: 'Example SP which should be excluded'
+          }
+        ]
+      end
+
+      let(:tagged_entities) do
+        expected_entities.map { |e| e.merge(tags: %w(idp aaf)) } +
+          extra_entities.map { |e| e.merge(tags: %w(sp aaf)) }
+      end
+
+      it 'excludes the SP entity' do
+        expect(rendered_entities.map { |e| e[:entity_id] })
+          .not_to include(extra_entities[0][:entity_id])
       end
     end
   end
